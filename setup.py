318b13e766fc5472b4c057d7fec59fa12950060f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-24 14:41:03 +0100

    add include field to nodepy.json, fix setup.py url and add description and author

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 setup(
   name='git-timetrack',
-  version='1.0.0',
+  version='1.0.1',
   license='MIT',
-  repository='https://github.com/NiklasRosenstein/git-timetrack',
+  description='Track work times in a separate timetracking branch.',
+  url='https://github.com/NiklasRosenstein/git-timetrack',
+  author='Niklas Rosenstein',
+  author_email='rosensteinniklas@gmail.com',
   packages=['git_timetrack'],
   entry_points={
     'console_scripts': [
